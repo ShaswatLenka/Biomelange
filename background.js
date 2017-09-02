8255c57f3b2ac60d713bc947f38d7b678bc24ee4
= <shaswatlenka@icloud.com>
2017-09-02 12:13:28 +0530

    added new features

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-//chrome.runtime.onMessage.addListener(function(response, tabId, sendMessage){
-  //  alert(response);
-//})
+chrome.runtime.onMessage.addListener(function(response, tabId, sendMessage){
+     for(var i = 0; i<response.length; i++){
+         var para = document.createElement("p");
+         var text = document.createTextNode(response[i]);
+         para.appendChild(text);
+         document.body.appendChild(para);
+     }
+})
+
+    
+    
 // addListener accepts a function that accepts three parameters: 
 //first paramenter  -> receives the message from content script.
 //second parameter  -> receives the tab information of the at page
--- popup.js
+++ popup.js
@@ -1,2 +1,22 @@
-//chrome.windows.create("userinfo.html");
+
+//creating the button 
+var button = document.createElement("button");
+button.innerHTML = "MIT News";
+
+//fetching the id
+var news = document.getElementById("news");
+news.appendChild("button");
+
+//adding event handler
+button.addEventListener("click", function(){
+   /*var iframe = document.createElement("iframe");
+    iframe.setAttribute("src","news.mit.edu/department/biological-engineering");
+    iframe.setAttribute("height", 100);
+    iframe.setAttribute("width", 100);
+    
+    var element = document.getElementById("news");
+    element.parentNode.insertBefore(iframe,element); */
+    
+    alert("button is working");
+});
 
